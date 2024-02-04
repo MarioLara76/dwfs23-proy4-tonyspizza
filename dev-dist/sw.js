fe960197f66262c249f9ca7416eb944125335909
MarioLara76 <48592956+MarioLara76@users.noreply.github.com>
2024-02-03 20:25:52 -0600

    PWA y Axios

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ugus7lu3t4"
+    "revision": "0.01a9pnujmb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
